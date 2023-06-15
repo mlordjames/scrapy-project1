a9de38063cef5a57b48ddb9659948ce91b3a6811
mlordJames <mlordjames@yahoo.com>
2023-06-15 16:36:08 +0100

    second commit

--- worldometer/worldometer/spiders/countries.py
+++ worldometer/worldometer/spiders/countries.py
@@ -7,4 +7,11 @@ class CountriesSpider(scrapy.Spider):
     start_urls = ["https://www.worldometers.info/world-population/population-by-country"]
 
     def parse(self, response):
-        pass
+        countries = response.xpath("//table[@id='example2']//td[2]/a")
+        for country in countries:
+            name = country.xpath(".//text()").get()
+            link = country.xpath(".//@href").get()
+
+            #yield scrapy.Request(url=f"https://www.worldometers.info{link}")
+            #yield scrapy.Request(url=response.urljoin(link))
+            yield response.follow(url=link)
