aa68d088fb3b97f814c427f19dccc24d04bf08f4
mlordJames <mlordjames@yahoo.com>
2023-06-17 16:02:45 +0100

    debugging for headers

--- tinydeal/tinydeal/spiders/special_offers.py
+++ tinydeal/tinydeal/spiders/special_offers.py
@@ -4,7 +4,12 @@ import scrapy
 class SpecialOffersSpider(scrapy.Spider):
     name = "special_offers"
     allowed_domains = ["web.archive.org"]
-    start_urls = ["https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html"]
+    #start_urls = ["https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html"]
+
+    def start_requests(self):
+        yield scrapy.Request(url='https://web.archive.org/web/20190225123327/https://www.tinydeal.com/specials.html', callback=self.parse, headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        })
 
     def parse(self, response):
         for product in response.xpath("//ul[@class='productlisting-ul']//li"):
@@ -13,5 +18,11 @@ class SpecialOffersSpider(scrapy.Spider):
                 "url":response.urljoin(product.xpath(".//a[@class='p_box_title']/@href").get()),
                 "discounted_price":product.xpath(".//span[@class='productSpecialPrice fl']/text()").get(),
                 "original_price":product.xpath(".//span[@class='normalprice fl']/text()").get(),
-                "image":response.urljoin(product.xpath(".//a[@class='p_box_img']/img/@src").get())
+                "image":response.urljoin(product.xpath(".//a[@class='p_box_img']/img/@src").get()),
+                #'User-Agent': response.request.headers['User-Agent']
             }
+        next_page = response.xpath("//a[@class='nextPage']/@href").get()
+        if next_page:
+            yield scrapy.Request(url=next_page, callback=self.parse,  headers={
+            'User-Agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/114.0.0.0 Safari/537.36'
+        })
